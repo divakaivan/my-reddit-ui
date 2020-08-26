38ed11c305940c87316458b9e09df8ac48277231
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-08-26 17:59:43 +0300

    add query for all posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
 import NavBar from "../components/NavBar";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div>hello world</div>
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
 
-export default Index;
+  return (
+    <>
+      <NavBar />
+      <div>hello world</div>
+      <br />
+      {!data ? (
+        <div>loading...</div>
+      ) : (
+        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+      )}
+    </>
+  );
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
