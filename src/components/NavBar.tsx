8de2e75bee9f7a378abde2a34e0e1b6542f41e41
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-08-26 17:06:32 +0300

    add a logout button and functionality

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Box, Link, Flex, Button } from "@chakra-ui/core";
 import NextLink from "next/link";
-import { useMeQuery } from "../generated/graphql";
+import { useMeQuery, useLogoutMutation } from "../generated/graphql";
 
 interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = () => {
+  const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
   const [{ data, fetching }] = useMeQuery();
   let body;
 
@@ -30,7 +31,13 @@ const NavBar: React.FC<NavBarProps> = () => {
     body = (
       <Flex>
         <Box mr={2}>{data.me.username}</Box>
-        <Button variant="link">logout</Button>
+        <Button
+          isLoading={logoutFetching}
+          onClick={() => logout()}
+          variant="link"
+        >
+          logout
+        </Button>
       </Flex>
     );
   }
