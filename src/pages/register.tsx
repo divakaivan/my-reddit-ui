f030a6758d6f28b4dc1905474d740684a9a0cc5f
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-08-26 15:41:54 +0300

    update register page to use the gql generated hook

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,44 +1,61 @@
 import React from "react";
-import {Form, Formik} from "formik";
-import {Wrapper} from "../components/Wrapper";
-import {InputField} from "../components/InputField";
-import {Box, Button} from "@chakra-ui/core/dist";
-import {useMutation} from "urql";
+import { Form, Formik } from "formik";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+import { Box, Button } from "@chakra-ui/core/dist";
+import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
 
 interface registerProps {}
 
-const REGISTER_MUT = `
-mutation Register($username: String!, $password: String!) {
-  register(options: {username: $username, password: $password}) {
-    errors {
-      field
-      message
-    }
-    user {
-      id
-      username
-    }
-  }  
-}`;
-
 const Register: React.FC<registerProps> = ({}) => {
-    const [{}] = useMutation(REGISTER_MUT);
+  const router = useRouter();
+  const [, register] = useRegisterMutation();
 
-    return (
-        <Wrapper variant="small">
-            <Formik initialValues={{username: "", password: ""}} onSubmit={values => console.log(values)}>
-                {({isSubmitting}) => (
-                    <Form>
-                        <InputField name="username" placeholder="username" label="Username"/>
-                        <Box mt={4}>
-                            <InputField name="password" placeholder="password" label="Password" type="password"/>
-                        </Box>
-                        <Button variantColor="teal" isLoading={isSubmitting} mt={4} type="submit">Register</Button>
-                    </Form>
-                )}
-            </Formik>
-        </Wrapper>
-    );
-}
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          // the values' keys align with what the mutation wants (username and password, thats why we can pass like that)
+          const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            // works
+            router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              variantColor="teal"
+              isLoading={isSubmitting}
+              mt={4}
+              type="submit"
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
 
 export default Register;
