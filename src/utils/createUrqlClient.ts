73a62bc649500c280555f7320c2548a34645c589
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-08-26 17:59:23 +0300

    add ssr to the page

---
+++ src/utils/createUrqlClient.ts
@@ -0,0 +1,66 @@
+import { fetchExchange, dedupExchange } from "urql";
+import { cacheExchange } from "@urql/exchange-graphcache";
+import {
+  LoginMutation,
+  MeQuery,
+  MeDocument,
+  RegisterMutation,
+  LogoutMutation,
+} from "../generated/graphql";
+import { betterUpdateQuery } from "./betterUpdateQuery";
+
+export const createUrqlClient = (ssrExchange: any) => ({
+  url: "http://localhost:4000/graphql",
+  fetchOptions: {
+    credentials: "include" as const,
+  },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                }
+                return {
+                  me: result.login.user,
+                };
+              }
+            );
+          },
+          register: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                }
+                return {
+                  me: result.register.user,
+                };
+              }
+            );
+          },
+          logout: (_result, args, cache, info) => {
+            betterUpdateQuery<LogoutMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              () => ({ me: null }) // update the cache so that me is null
+            );
+          },
+        },
+      },
+    }),
+    ssrExchange,
+    fetchExchange,
+  ],
+});
