73e05682551f625f522939ee92781de0a836ad2e
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-08-26 13:50:39 +0300

    add urql provider

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import {ThemeProvider, CSSReset, ColorModeProvider} from '@chakra-ui/core'
+import {Provider, createClient} from "urql";
+
+import theme from '../theme'
+
+const client = createClient({url: 'http://localhost:4000/graphql'});
+
+function MyApp({Component, pageProps}) {
+    return (
+        <Provider value={client}>
+            <ThemeProvider theme={theme}>
+                <ColorModeProvider>
+                    <CSSReset/>
+                    <Component {...pageProps} />
+                </ColorModeProvider>
+            </ThemeProvider>
+        </Provider>
+    )
+}
+
+export default MyApp
