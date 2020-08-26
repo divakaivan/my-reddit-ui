ebdf4a3ac3cdbe914569c515ffd5a4069406be68
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-08-26 15:39:52 +0300

    remove the themes. was giving problem.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,22 @@
-import {ThemeProvider, CSSReset, ColorModeProvider} from '@chakra-ui/core'
-import {Provider, createClient} from "urql";
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { Provider, createClient } from "urql";
 
-import theme from '../theme'
+import theme from "../theme";
 
-const client = createClient({url: 'http://localhost:4000/graphql'});
+const client = createClient({
+  url: "http://localhost:4000/graphql",
+  fetchOptions: { credentials: "include" },
+});
 
-function MyApp({Component, pageProps}) {
-    return (
-        <Provider value={client}>
-            <ThemeProvider theme={theme}>
-                <ColorModeProvider>
-                    <CSSReset/>
-                    <Component {...pageProps} />
-                </ColorModeProvider>
-            </ThemeProvider>
-        </Provider>
-    )
+function MyApp({ Component, pageProps }: any) {
+  return (
+    <Provider value={client}>
+      <ThemeProvider theme={theme}>
+        <CSSReset />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
