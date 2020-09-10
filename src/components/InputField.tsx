746ec4f9dbcee6afea185871491cbc9c38873ae3
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-09-10 15:45:33 +0300

    allow for the field to be either input or textarea

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -6,10 +6,12 @@ import {
   Input,
   FormErrorMessage,
 } from "@chakra-ui/core";
+import {Textarea} from "@chakra-ui/core/dist";
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   name: string;
+  textarea?: boolean;
 };
 
 // '' => false
@@ -17,14 +19,17 @@ type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
 
 export const InputField: React.FC<InputFieldProps> = ({
   label,
+    textarea,
   size: _,
   ...props
 }) => {
+  let InputOrTextarea = Input;
+  if (textarea) InputOrTextarea = Textarea;
   const [field, { error }] = useField(props);
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input {...field} {...props} id={field.name} />
+      <InputOrTextarea {...field} {...props} id={field.name} />
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
   );
