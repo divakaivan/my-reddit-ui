6f0b5eb90ec32edb20d3c0fa6e2b9963c6065071
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-09-10 15:44:25 +0300

    use the layout component and add a create post button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,28 @@
 import React from "react";
-import { NavBar } from "../components/NavBar";
-import { withUrqlClient } from "next-urql";
-import { createUrqlClient } from "../utils/createUrqlClient";
-import { usePostsQuery } from "../generated/graphql";
+import {withUrqlClient} from "next-urql";
+import {createUrqlClient} from "../utils/createUrqlClient";
+import {usePostsQuery} from "../generated/graphql";
+import Layout from "../components/Layout";
+import {Link} from "@chakra-ui/core/dist";
+import NextLink from "next/link";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery();
-  return (
-    <>
-      <NavBar />
-      <div>hello world</div>
-      <br />
-      {!data ? (
-        <div>loading...</div>
-      ) : (
-        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
-      )}
-    </>
-  );
+    const [{data}] = usePostsQuery();
+    return (
+        <Layout>
+            <NextLink href="/create-post">
+                <Link>
+                    create post
+                </Link>
+            </NextLink>
+            <br/>
+            {!data ? (
+                <div>loading...</div>
+            ) : (
+                data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+            )}
+        </Layout>
+    );
 };
 
-export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
+export default withUrqlClient(createUrqlClient, {ssr: true})(Index);
