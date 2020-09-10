1fda60e24ccb86429ca198629f71b7900683c4f0
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-09-10 15:44:01 +0300

    after login, push to previous page (if requested), or push to home by default

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,65 +1,70 @@
 import React from "react";
-import { Formik, Form } from "formik";
-import { Box, Button, Link, Flex } from "@chakra-ui/core";
-import { Wrapper } from "../components/Wrapper";
-import { InputField } from "../components/InputField";
-import { useLoginMutation } from "../generated/graphql";
-import { toErrorMap } from "../utils/toErrorMap";
-import { useRouter } from "next/router";
-import { withUrqlClient } from "next-urql";
-import { createUrqlClient } from "../utils/createUrqlClient";
+import {Formik, Form} from "formik";
+import {Box, Button, Link, Flex} from "@chakra-ui/core";
+import {Wrapper} from "../components/Wrapper";
+import {InputField} from "../components/InputField";
+import {useLoginMutation} from "../generated/graphql";
+import {toErrorMap} from "../utils/toErrorMap";
+import {useRouter} from "next/router";
+import {withUrqlClient} from "next-urql";
+import {createUrqlClient} from "../utils/createUrqlClient";
 import NextLink from "next/link";
 
 const Login: React.FC<{}> = ({}) => {
-  const router = useRouter();
-  const [, login] = useLoginMutation();
-  return (
-    <Wrapper variant="small">
-      <Formik
-        initialValues={{ usernameOrEmail: "", password: "" }}
-        onSubmit={async (values, { setErrors }) => {
-          const response = await login(values);
-          if (response.data?.login.errors) {
-            setErrors(toErrorMap(response.data.login.errors));
-          } else if (response.data?.login.user) {
-            // worked
-            router.push("/");
-          }
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <InputField
-              name="usernameOrEmail"
-              placeholder="username or email"
-              label="Username or Email"
-            />
-            <Box mt={4}>
-              <InputField
-                name="password"
-                placeholder="password"
-                label="Password"
-                type="password"
-              />
-            </Box>
-            <Flex mt={2}>
-              <NextLink href="/forgot-password">
-                <Link ml="auto">forgot password?</Link>
-              </NextLink>
-            </Flex>
-            <Button
-              mt={4}
-              type="submit"
-              isLoading={isSubmitting}
-              variantColor="teal"
+    const router = useRouter();
+    const [, login] = useLoginMutation();
+    return (
+        <Wrapper variant="small">
+            <Formik
+                initialValues={{usernameOrEmail: "", password: ""}}
+                onSubmit={async (values, {setErrors}) => {
+                    const response = await login(values);
+                    if (response.data?.login.errors) {
+                        setErrors(toErrorMap(response.data.login.errors));
+                    } else if (response.data?.login.user) {
+                        if (typeof router.query.next === 'string') {
+                            // push to a specific page if we have a next page
+                            router.push(router.query.next);
+                        } else {
+                            // push to home page
+                            router.push("/");
+                        }
+                    }
+                }}
             >
-              login
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </Wrapper>
-  );
+                {({isSubmitting}) => (
+                    <Form>
+                        <InputField
+                            name="usernameOrEmail"
+                            placeholder="username or email"
+                            label="Username or Email"
+                        />
+                        <Box mt={4}>
+                            <InputField
+                                name="password"
+                                placeholder="password"
+                                label="Password"
+                                type="password"
+                            />
+                        </Box>
+                        <Flex mt={2}>
+                            <NextLink href="/forgot-password">
+                                <Link ml="auto">forgot password?</Link>
+                            </NextLink>
+                        </Flex>
+                        <Button
+                            mt={4}
+                            type="submit"
+                            isLoading={isSubmitting}
+                            variantColor="teal"
+                        >
+                            login
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
+        </Wrapper>
+    );
 };
 
 export default withUrqlClient(createUrqlClient)(Login);
