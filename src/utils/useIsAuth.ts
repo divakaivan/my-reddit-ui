434629a0420da972effe28cefc2ed5c1b9c5f6a2
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-09-11 20:34:54 +0300

    switch useIsAuth to apollo

--- src/utils/useIsAuth.ts
+++ src/utils/useIsAuth.ts
@@ -1,15 +1,15 @@
-import {useEffect} from "react";
-import {useMeQuery} from "../generated/graphql";
-import {useRouter} from "next/router";
+import { useEffect } from "react";
+import { useMeQuery } from "../generated/graphql";
+import { useRouter } from "next/router";
 
 export const useIsAuth = () => {
-    const [{data, fetching}] = useMeQuery();
-    const router = useRouter();
-    useEffect(()=>{
-        if (!fetching && !data?.me) {
-            // go to login if not logged in when we come to this page
-            router.replace("/login?next=" + router.pathname)
-            // after user log in, take the user to the given page
-        }
-    }, [data, router]);
+  const { data, loading } = useMeQuery();
+  const router = useRouter();
+  useEffect(() => {
+    if (!loading && !data?.me) {
+      // go to login if not logged in when we come to this page
+      router.replace("/login?next=" + router.pathname);
+      // after user log in, take the user to the given page
+    }
+  }, [data, router]);
 };
