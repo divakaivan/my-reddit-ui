a703be96b231146dd835c9a5bb2819bddf49f48f
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-09-12 19:26:07 +0300

    create withApollo client and also includes pagination magic

---
+++ src/utils/withApollo.ts
@@ -0,0 +1,38 @@
+import { createWithApollo } from "./createWithApollo";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { PaginatedPosts } from "../generated/graphql";
+import { NextPageContext } from "next";
+
+const createClient = (ctx: NextPageContext) =>
+  new ApolloClient({
+    uri: "http://localhost:4000/graphql",
+    credentials: "include",
+    headers: {
+      cookie:
+        (typeof window === "undefined" ? ctx.req?.headers.cookie : undefined) ||
+        "",
+    },
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            posts: {
+              keyArgs: [],
+              merge(
+                existing: PaginatedPosts | undefined,
+                incoming: PaginatedPosts
+              ): PaginatedPosts {
+                return {
+                  __typename: "PaginatedPosts", // these two === ...incoming
+                  hasMore: incoming.hasMore,
+                  posts: [...(existing?.posts || []), ...incoming.posts],
+                };
+              },
+            },
+          },
+        },
+      },
+    }),
+  });
+
+export const withApollo = createWithApollo(createClient);
