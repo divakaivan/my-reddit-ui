195e4b5285267707e7431ad670dd16daa8802e64
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-09-12 19:26:27 +0300

    create-post page switch to apollo

--- src/pages/create-post.tsx
+++ src/pages/create-post.tsx
@@ -1,59 +1,57 @@
 import React from "react";
-import {Form, Formik} from "formik";
-import {InputField} from "../components/InputField";
-import {Box, Button} from "@chakra-ui/core/dist";
-import {useCreatePostMutation} from "../generated/graphql";
-import {useRouter} from "next/router";
-import {createUrqlClient} from "../utils/createUrqlClient";
-import {withUrqlClient} from "next-urql";
+import { Form, Formik } from "formik";
+import { InputField } from "../components/InputField";
+import { Box, Button } from "@chakra-ui/core/dist";
+import { useCreatePostMutation } from "../generated/graphql";
+import { useRouter } from "next/router";
 import Layout from "../components/Layout";
-import {useIsAuth} from "../utils/useIsAuth";
-
+import { useIsAuth } from "../utils/useIsAuth";
+import { withApollo } from "../utils/withApollo";
 
 const CreatePost: React.FC<{}> = ({}) => {
-    useIsAuth();
-    // we use the me query couple of times, but they dont cast a request each time because urql caches the result
-    const [, createPost] = useCreatePostMutation();
-    const router = useRouter();
+  useIsAuth();
+  // we use the me query couple of times, but they dont cast a request each time because urql caches the result
+  const [createPost] = useCreatePostMutation();
+  const router = useRouter();
 
-    return (
-        <Layout variant='small'>
-            <Formik
-                initialValues={{ title: '', text: '' }}
-                onSubmit={async (values) => {
-                    const {error} = await createPost({input: values});
-                    if (!error) router.push("/")
-
-                }}
+  return (
+    <Layout variant="small">
+      <Formik
+        initialValues={{ title: "", text: "" }}
+        onSubmit={async (values) => {
+          const { errors } = await createPost({
+            variables: { input: values },
+            update: (cache) => {
+              cache.evict({ fieldName: "posts:{}" });
+            },
+          });
+          if (!errors) router.push("/");
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name="title" placeholder="title" label="Title" />
+            <Box mt={4}>
+              <InputField
+                textarea
+                name="text"
+                placeholder="text..."
+                label="Body"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              variantColor="teal"
             >
-                {({ isSubmitting }) => (
-                    <Form>
-                        <InputField
-                            name="title"
-                            placeholder="title"
-                            label="Title"
-                        />
-                        <Box mt={4}>
-                            <InputField
-                                textarea
-                                name="text"
-                                placeholder="text..."
-                                label="Body"
-                            />
-                        </Box>
-                        <Button
-                            mt={4}
-                            type="submit"
-                            isLoading={isSubmitting}
-                            variantColor="teal"
-                        >
-                            create post
-                        </Button>
-                    </Form>
-                )}
-            </Formik>
-        </Layout>
-    )
+              create post
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Layout>
+  );
 };
 
-export default withUrqlClient(createUrqlClient)(CreatePost); // needed so we can use the generated mutations
+export default withApollo({ ssr: false })(CreatePost);
